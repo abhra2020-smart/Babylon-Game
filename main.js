6403d9ef826b4b3776cba4314dda9bbfef00088a
abhra2020-smart <68565872+abhra2020-smart@users.noreply.github.com>
2020-08-05 20:13:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,43 +6,34 @@ window.addEventListener('DOMContentLoaded', function(){
             var engine = new BABYLON.Engine(canvas, true);
 
             // createScene function that creates and return the scene
-           var createScene = function () {
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+            var createScene = function(){
+                // create a basic BJS Scene object
+                var scene = new BABYLON.Scene(engine);
 
-    // sun color
-    var r = 237
+                // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+                var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+                // target the camera to scene origin
+                camera.setTarget(BABYLON.Vector3.Zero());
 
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+                // attach the camera to the canvas
+                camera.attachControl(canvas, false);
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+                // create a basic light, aiming 0,1,0 - meaning, to the sky
+                var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 1), scene);
+                // create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+                var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+                // move the sphere upward 1/2 of its height
+                sphere.position.y = 1;
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-    var sun = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+                // create a built-in "ground" shape;
+                var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
 
-    // make the sun
-    sun.position.y = 45;
-
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 10, height: 10}, scene);
-
-    return scene;
-
-};
+                // return the created scene
+                return scene;
+            }
 
             // call the createScene function
             var scene = createScene();
